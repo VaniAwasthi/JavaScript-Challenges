c2c794e85836e3fce6f5b261d661ecb2b13653c6
Vani Awasthi <awasthivani95@gmail.com>
2023-02-28 14:26:38 +0530

    Add existing project files to Git

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
+//current Date and Time//
+//solution:1//
 function generatingCurrentDateTime() {
 var today = new Date();
 var date = today.getDate();
@@ -5,3 +7,26 @@ var month = today.getMonth()+1;
 var year = today.getFullYear();
 document.getElementById("currentDateTime").innerHTML = today;
  }
+ //end of solution 1//
+
+ //Given a string, reverse each word in the sentence//
+//solution:2//
+ function reverseString() {
+    input = document.getElementById("inputText").value;
+    if(input == '') {
+    document.getElementById("result").style.color = "red";
+    document.getElementById("result").innerHTML = "Please Enter a Valid Text ";
+    return;
+    }
+    let reverseResult = "";
+    document.getElementById("result").innerHTML = '';
+    var i = input.length -1;
+    while ( i >= 0) {
+    reverseResult = reverseResult + input[i];
+    i--;
+    }
+    document.getElementById("result").style.color = "blue";
+    document.getElementById("result").innerHTML = "Reverse strinh :" + reverseResult;
+    }
+
+     //end of solution 1//
