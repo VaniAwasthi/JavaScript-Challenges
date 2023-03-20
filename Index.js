9aa1d29e30f18021b2467993442770921f2bae09
Vani Awasthi <awasthivani95@gmail.com>
2023-03-20 23:29:06 +0530

    Add existing project files to Git

--- Index.js
+++ Index.js
@@ -106,4 +106,13 @@ function positiveSum() {
 
 }  
   //end of solution//
+  var input=document.getElementById("statement").value
+  function capitalize(input) {  
+    var words = input.split(' ');  
+    var CapitalizedWords = [];  
+    words.forEach(element => {  
+        CapitalizedWords.push(element[0].toUpperCase() + element.slice(1, element.length));  
+    });  
+    return CapitalizedWords.join(' ');  
+}  
 
