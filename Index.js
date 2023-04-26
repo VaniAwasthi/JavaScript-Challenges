0157b26c27804d36d2332ea966d4f96d308cfba6
Vani Awasthi <awasthivani95@gmail.com>
2023-04-26 18:56:08 +0530

    Adding new question

--- Index.js
+++ Index.js
@@ -198,7 +198,13 @@ function fizzBuzz() {
     }
     return i;
    }
-
+//// Write a function that takes two strings (a and b) as arguments // If a contains b, append b to the beginning of a // If not, append it to the end // Return the concatenation Add Answer
+var a=document.getElementById("1").value
+var b =document.getElementById("2").value
+function
+myFunction(a, b) {
+return a.includes(b) ? b + a : a + b;
+}
 
 
   
