2782896ac103bff6df53525b403baf3bec1a8506
Vani Awasthi <awasthivani95@gmail.com>
2023-04-28 15:15:24 +0530

    adding question

--- Index.js
+++ Index.js
@@ -216,7 +216,23 @@ function checkingEquallity(arr){
   document.getElementById("equall").innerHTML=equLL
 
 }
-
-
-  
-
+//end of solution
+//Once you click the button, it should stop moving. When you click it again, it should move again.
+//solution 19//
+const button = document.getElementById('button');
+let stopped = false;
+ 
+function move(isReturning) {
+ const width = button.parentNode.clientWidth;
+ const left = parseInt(button.style.left , 10) || 0;
+ if (!stopped) {
+    button.style.left = (isReturning ? left - 1 : left + 1) + 'px';
+    setTimeout(() => move ((isReturning && left > 0) || left === width - button.clientWidth ), 10);
+ };
+};
+ 
+move();
+button.addEventListener('click', () => {
+stopped = !stopped;
+	move();
+});
