076f133b72bfbbb7aa7fd914c0e09efe223041bd
Vani Awasthi <awasthivani95@gmail.com>
2023-04-28 17:01:56 +0530

    adding new questions

--- Index.js
+++ Index.js
@@ -217,22 +217,10 @@ function checkingEquallity(arr){
 
 }
 //end of solution
-//Once you click the button, it should stop moving. When you click it again, it should move again.
+//Write a function that takes an object (a) as argument. Return the sum of all object values.
 //solution 19//
-const button = document.getElementById('button');
-let stopped = false;
- 
-function move(isReturning) {
- const width = button.parentNode.clientWidth;
- const left = parseInt(button.style.left , 10) || 0;
- if (!stopped) {
-    button.style.left = (isReturning ? left - 1 : left + 1) + 'px';
-    setTimeout(() => move ((isReturning && left > 0) || left === width - button.clientWidth ), 10);
- };
-};
- 
-move();
-button.addEventListener('click', () => {
-stopped = !stopped;
-	move();
-});
+ function objAddition(a){
+  return Object.values(a).reduce((sum, cur) => sum + cur, 0);
+  
+ }
+
