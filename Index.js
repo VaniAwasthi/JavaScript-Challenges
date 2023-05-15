3803d20000605be4d0b943a7c0fd7436233b8e5f
Vani Awasthi <awasthivani95@gmail.com>
2023-05-15 17:46:58 +0530

    adding new question

--- Index.js
+++ Index.js
@@ -333,3 +333,31 @@ function longest_str_in_array()
 }
 //end of solution//
 //Write a function that takes a string and returns the character that is most commonly used in the string.
+const test = () => { 
+  var str=document.getElementById("set").value;
+  if (str.length === 0) 
+   {
+   return 'String should not be empty!'
+  }
+   if (typeof str !== 'string')
+    {
+      return 'It must be a string.'
+    }
+ const occurrence_Map = new Map()
+
+ for (const char of str) 
+   {
+     occurrence_Map.set(char, occurrence_Map.get(char) + 1 || 1)
+   }
+
+ // find the max char from the occurrence map
+ let max_char = { char: '', occur: -Infinity }
+
+ for (const [char, occur] of occurrence_Map) {
+   if (occur > max_char.occur) {
+     max_char = { char,occur }
+   }
+ }
+
+ console.log(max_char.char) 
+}
