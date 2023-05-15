5e04d78074aaf28b75e2617a530b3c83e8027fff
Vani Awasthi <awasthivani95@gmail.com>
2023-05-15 16:51:41 +0530

    adding some cj=hanges

--- Index.js
+++ Index.js
@@ -314,3 +314,19 @@ document.getElementById("alpha2").innerHTML="Go to console for result"
 console.log(string);
 }
 
+//end of solution//
+
+function longest_str_in_array()
+  {
+    var arra= ["abc", "ab" , "bcd"]
+    let max_str = arra[0].length;
+    let ans = arra[0];
+    for (let i = 1; i < arra.length; i++) {
+        const maxi = arra[i].length;
+        if (maxi > max_str) {
+            ans = arra[i];
+            max_str = maxi;
+        }
+    }
+    console.log(ans);
+}
