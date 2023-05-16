80bff64e40229a331093ada9e3ff6c74367fe70d
Vani Awasthi <awasthivani95@gmail.com>
2023-05-16 15:24:58 +0530

    aading sme change

--- Index.js
+++ Index.js
@@ -365,13 +365,8 @@ document.getElementById("pra").innerHTML="see console for result"
 // program to check if the string is palindrome or not
 
 function palindrome(string) {
-  // find the length of a string
   const len = string.length;
-
-  // loop through half of the string
   for (let i = 0; i < len / 2; i++) {
-
-      // check if first and last string are same
       if (string[i] !== string[len - 1 - i]) {
           return 'It is not a palindrome';
       }
